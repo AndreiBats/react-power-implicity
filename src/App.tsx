5fe61f22497c008b9b4fc02ec33086b44fd1c5ba
AndreiBats <1158369@tut.by>
2022-08-01 14:07:25 +0300

    feat: add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Navbar } from "./components/Navbar/Navbar";
+import { Slider } from "./components/Slider/Slider";
+import "./app.scss";
+
 export function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Slider />
+    </div>
+  );
 }
