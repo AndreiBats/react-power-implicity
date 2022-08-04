fa57824a2da611d853a5a5ca4fd2be09ced46fd5
AndreiBats <1158369@tut.by>
2022-08-04 13:07:27 +0300

    fix: fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Navbar } from "./components/Navbar/Navbar";
 import { Slider } from "./components/Slider/Slider";
 import "./app.scss";
-import "./index.scss";
 
 export const App = () => {
   return (
